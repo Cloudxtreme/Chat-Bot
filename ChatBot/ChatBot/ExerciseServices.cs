075eaeb46e17fb54813a5360c2db2d8ed28d7775
Matthew Martin <matthewdeanmartin@gmail.com>
2015-08-26 16:46:59 -0400

    Starting to add tests.

---
+++ ChatBot/ChatBot/ExerciseServices.cs
@@ -0,0 +1,19 @@
+﻿using ChatBot.ndfdXML;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatBot
+{
+    
+    public class ExerciseServices
+    {
+        public void Something() {
+            ndfdXMLPortTypeClient client = new ndfdXMLPortTypeClient();
+            string value= client.LatLonListZipCode("20912");
+
+        }
+    }
+}
---
+++ ChatBot/UnitTests/ExerciseServices.cs
@@ -0,0 +1,22 @@
+﻿using ChatBot.ndfdXML;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using ChatBot;
+
+namespace UnitTests
+{
+    [TestFixture]
+    public class ExerciseServices
+    {
+        [Test]
+        public void Something() {
+            ndfdXMLPortTypeClient client = new ndfdXMLPortTypeClient();
+            string value= client.LatLonListZipCode("20912");
+            Console.WriteLine(value);
+        }
+    }
+}
