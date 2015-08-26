35bffbdd6ac68eaa55da6c895ab1dbe678a4fb96
Matthew Martin <matthewdeanmartin@gmail.com>
2015-08-26 17:37:31 -0400

    Console App

---
+++ ChatBot/ConsoleClient/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ChatBot;
+
+namespace ConsoleClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Weather client = new Weather();
+            client.ForecastWeather();
+        }
+    }
+}
